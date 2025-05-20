089638fd5bc24f7093c1be715a7213f1cb926494
OreCarlos <carlosoreprogramador@gmail.com>
2025-05-19 19:14:25 -0500

    banner subido falta arreglar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+
+
+<head>
+    <link rel="stylesheet" href="{{ asset('css/index_css/index.css') }}">
+    {{-- ipervinculo para iconos xde --}}
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+
+</head>
+
+<body>
+
+
+    <section class="header_banner">
+        
+        <img class="img_header" src="{{ asset('img/index/img1.png') }}" alt="Productos reciclables">
+
+
+        <article class="texto_header">
+            <h1>Transformamos<br>
+                Residuos
+               <br> <span class="h2_texto"> en Futuro Recicla con ECOCREATIONS</span> 
+            </h1>
+            
+            <a href="#" class="btn_vermas">
+                VER MÁS
+                <i class="fas fa-arrow-right"></i>
+            </a>
+        </article>
+
+    </section>
+
+
+    
+
+</body>
+
+</html>
